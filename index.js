b138456faa49a50b97a1d6d17a2e52a2755f6920
heriyanto <heriyanto@heriyantos-MacBook-Pro.local>
2018-01-21 18:07:28 +0700

    change version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! v5');
+  res.send('Hello World!');
 });
 
 var server = app.listen(3000, function () {
